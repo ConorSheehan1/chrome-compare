9372bfff1e04b81bd01afeb0e2dc9071a06e87d9
conor <conor.sheehan@live.ie>
2018-04-17 21:59:25 +0100

    node options_page_feature.js working with packed extension, start to restructure readme

--- features/options_page_feature.js
+++ features/options_page_feature.js
@@ -1,16 +1,18 @@
 "use strict"; 
-let webdriver = require("selenium-webdriver"),
-SeleniumServer = require("selenium-webdriver/remote").SeleniumServer;
+let webdriver = require("selenium-webdriver");
+let chrome = require('selenium-webdriver/chrome');
 
-let caps = {
-    'name': 'Chrome extension options page test',
-    'build': '1.0',
-    'browserName': 'google chrome'
+function encode(file) {
+    var stream = require('fs').readFileSync(file);
+    return new Buffer(stream).toString('base64');
 }
 
+let options = new chrome.Options();
+options.addExtensions(encode('../extension.crx'));
+
 let driver = new webdriver.Builder()
-    // .usingServer(cbtHub)
-    .withCapabilities(caps)
-    .build(); 
+    .forBrowser('chrome')
+    .setChromeOptions(options)
+    .build();
 
 driver.get("chrome://extensions");
