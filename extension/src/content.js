485489191d86ebfe42711040df3d83919cb8cc99
Conor Sheehan <conor.sheehan.dev@gmail.com>
2019-09-29 18:26:05 +0100

    eslint autofix.

--- extension/src/content.js
+++ extension/src/content.js
@@ -1,8 +1,8 @@
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    let default_base_urls = ["https://stackoverflow.com", "https://askubuntu.com", "https://datascience.stackexchange.com"];
-    chrome.storage.sync.get({'base_urls' : default_base_urls}, function (result) {
+  (request, sender, sendResponse) => {
+    const default_base_urls = ['https://stackoverflow.com', 'https://askubuntu.com', 'https://datascience.stackexchange.com'];
+    chrome.storage.sync.get({ base_urls: default_base_urls }, (result) => {
       open_urls(window.location.href, result.base_urls, request, chrome.runtime.sendMessage);
     });
-  }
+  },
 );
--- extension/src/url_parser.js
+++ extension/src/url_parser.js
@@ -1,51 +1,49 @@
 function build_regex(base_urls) {
   // build regex with look ahead that will match any of the base urls
-  // and then add whatever comes after the baseurl to the last capture group 
-  let regex_string = "(" + base_urls.join("|") + ")(?=(\.*))";
-  return new RegExp(regex_string, "i");
+  // and then add whatever comes after the baseurl to the last capture group
+  const regex_string = `(${base_urls.join('|')})(?=(\.*))`;
+  return new RegExp(regex_string, 'i');
 }
 
-//http://test.com(/the/path/gets/matched)
+// http://test.com(/the/path/gets/matched)
 function get_path(regex_match) {
-  return regex_match[regex_match.length-1];
+  return regex_match[regex_match.length - 1];
 }
 
-//(http://this.gets.matched)/some/path/not/matched
+// (http://this.gets.matched)/some/path/not/matched
 function get_domain(regex_match) {
   return regex_match[0];
 }
 
-function base_urls_except(base_urls, except){
-  return base_urls.filter(url => url != except);
+function base_urls_except(base_urls, except) {
+  return base_urls.filter((url) => url != except);
 }
 
 function open_urls(current_url, base_urls, request, callback_to_open_url) {
-  let matches = current_url.match(build_regex(base_urls));
+  const matches = current_url.match(build_regex(base_urls));
 
   if (matches) {
-    let url_path = get_path(matches);
+    const url_path = get_path(matches);
     // get all base urls except the current url and add the path to the end of them
-    let urls_to_open = base_urls_except(base_urls, get_domain(matches)).map(url => url + url_path);
+    const urls_to_open = base_urls_except(base_urls, get_domain(matches)).map((url) => url + url_path);
 
-    if (confirm("open the following urls?" + urls_to_open.map(url => "\n" + url)) 
-      && request.message === "clicked_browser_action") {
-      urls_to_open.forEach( function(url) {
-        callback_to_open_url({"message": "open_new_tab", "url": url});
+    if (confirm(`open the following urls?${urls_to_open.map((url) => `\n${url}`)}`)
+      && request.message === 'clicked_browser_action') {
+      urls_to_open.forEach((url) => {
+        callback_to_open_url({ message: 'open_new_tab', url });
       });
     }
   } else {
-    alert("no match");
+    alert('no match');
   }
 }
 
 // Export node module.
-if ( typeof module !== 'undefined' && module.hasOwnProperty('exports') )
-{
-    module.exports = {
-      open_urls: open_urls,
-      build_regex: build_regex,
-      base_urls_except: base_urls_except,
-      get_path: get_path
-    }
+if (typeof module !== 'undefined' && module.hasOwnProperty('exports')) {
+  module.exports = {
+    open_urls,
+    build_regex,
+    base_urls_except,
+    get_path,
+  };
 }
-
